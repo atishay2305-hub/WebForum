3b7e27c8e4c81accb3be8358cca3bbf4bf2352c5
nouman <syednouman2114@gmail.com>
2023-04-25 19:54:19 -0400

    setup.sh done

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "├─ Installing dependencies"
+echo "├─ flask"
+pip install flask
+echo "├─ pymongo"
+pip install pymongo
+echo "├─ secrets"
+pip install secrets
+# idk if we should use pip or pip3, or just do both
+# pip3 install flask
+# pip3 install pymongo
+# pip3 install secrets
+
+echo "└─ Dependencies successfully installed!"
